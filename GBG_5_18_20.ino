cb6087d9c03a5b9327a7b2346dcbc71d9649a82e
DrChad <cmbaker@cbu.edu>
2020-05-19 11:00:25 -0500

    Add files via upload

--- GBG_5_18_20.ino
+++ GBG_5_18_20.ino
@@ -1,4 +1,5 @@
-// Copyright (c) 2020, Judit Aubets Macia, Steve Whatley, Chad Baker, and John Ventura // All rights reserved.
+// Copyright (c) 2020, Judit Aubets Macia, Steve Whatley, Chad Baker, and John Ventura
+// All rights reserved.
 // This program may be freely copied and modified provided that the above copyright is included.
 
 #include <CytronMotorDriver.h>  //library for MDD10A
